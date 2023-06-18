2c3165a1eb85433abd0484cba05ad409425406ae
Codeprince001 <urangyorkwisdom@gmail.com>
2023-06-18 10:33:29 +0100

    modified custom datastorage

--- src/Context/GlobalState.js
+++ src/Context/GlobalState.js
@@ -3,8 +3,6 @@ import { AppReducer } from '../Context/AppReducer';
 
 const initialState = {
   transactions: [
-    { id: 1, text: "Flower", amount: -200 },
-    { id: 2, text: "Salary", amount: 20000 },
   ]
 };
 
